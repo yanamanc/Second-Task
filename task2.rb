b43cf3f8578b290414caa4f3c1fd03576cf7f010
Yana Mancevich <yaninamantsevich@mail.ru>
2018-11-09 20:03:16 +0200

    Final

--- task2.rb
+++ task2.rb
@@ -2,7 +2,7 @@ my_array = []
 i = 10
 
 while i <= 100
-  my_array.push(i)
+  my_array << i
   i += 5
 end
 
--- task5.rb
+++ task5.rb
@@ -2,6 +2,6 @@ vowels = %w(a e o u i)
 result = {} 
 
 ("a".."z").each.with_index(1) do |item, index|
-  	result[index] = item if vowels.include?(item)
+  result[index] = item if vowels.include?(item)
 end
 puts "#{result}"
